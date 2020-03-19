5e7773696d303295ef056f307dddf090e6f96c69
rrTong <rrtong@ucdavis.edu>
2020-03-18 18:21:55 -0700

    listen for discord mentions

--- brucelee/bruce.js
+++ brucelee/bruce.js
@@ -22,7 +22,7 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-    if (message.match(/bruce/i) || message.match(/junfan/i) || message.match(/jun fan/i) || message.match(/小龍/i) || message.match(/小龙/i) || message.match(/振藩/i) || message.match(/ブルース/i) || message.match(/소룡/i) || message.match(/Tiểu Long/i) || message.match(/Chấn Phiên/i) || message.match(/Брюс/i)) {
+    if (message.match(bot.id) || message.match(/bruce/i) || message.match(/junfan/i) || message.match(/jun fan/i) || message.match(/小龍/i) || message.match(/小龙/i) || message.match(/振藩/i) || message.match(/ブルース/i) || message.match(/소룡/i) || message.match(/Tiểu Long/i) || message.match(/Chấn Phiên/i) || message.match(/Брюс/i)) {
         let quoteRandom = quotes[parseInt(Math.random() * quotes.length)];
         bot.sendMessage({
             to: channelID,
